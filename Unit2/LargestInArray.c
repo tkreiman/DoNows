c634b4d66a1ee3e850366dcb551c76f580ee3ef0
Toby Kreiman <tobykreiman@gmail.com>
2018-10-24 17:22:59 +0000

    largest in array

--- Unit2/LargestInArray.c
+++ Unit2/LargestInArray.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <cs50.h>
 
-// Fix the errors in this code that prevent it from printing all the elements in array
+// Fix the errors in this code that prevent it from printing the largest element in array
+// Assuming all values in array are positive
 
 bool is_prime(int n);
 
@@ -9,10 +10,15 @@ int main(void)
 {
     int array[] = {2, 5, 2, 27, 21, 1, 89, 27};
     // Eight elements in array
-    int length = 8;
+    int length = 7;
 
+    int largest = -1;
     for(int i = 1; i <= length; i++)
     {
-        printf("%i\n", array[i]);
+        if(largest < array[i])
+        {
+            largest = array[i];
+        }
     }
+    printf("%i\n", largest);
 }
