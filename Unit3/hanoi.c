f318e2690a648ccc744a37f8eb21f0ad16b86bc3
Toby Kreiman <tobykreiman@gmail.com>
2018-11-18 18:21:10 +0000

    hanoi problem

--- Unit3/hanoi.c
+++ Unit3/hanoi.c
@@ -5,7 +5,11 @@
 // Implement an algorithm that prints out the moves to solve Towers of Hanoi
 // https://www.mathsisfun.com/games/towerofhanoi.html
 // There will be one command line argument saying the starting number of disks
-// Make sure to check for invalid arguments
+// A first tower, B is middle tower, C is right tower
+// A move should be printed as:
+// A to B.
+// Note the space after the period for the check to work
+
 
 void MoveTower(int disk, string start, string end, string open);
 
@@ -14,5 +18,24 @@ void MoveTower(int disk, string start, string end, string open);
 int main(int argc, char *argv[])
 {
     // Check for invalid arguments
-    MoveTower(num_disks, "A", "B", "C")
+    if (argc != 2)
+    {
+        return 1;
+    }
+    int num_disks = atoi(argv[1]);
+    MoveTower(num_disks - 1, "A", "C", "B");
+}
+
+void MoveTower(int disk, string start, string end, string open)
+{
+    if (disk == 0)
+    {
+        printf("%s to %s. ", start, end);
+    }
+    else
+    {
+        MoveTower(disk - 1, start, open, end);
+        printf("%s to %s. ", start, end);
+        MoveTower(disk - 1, open, end, start);
+    }
 }
