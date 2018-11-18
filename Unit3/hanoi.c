c1bfb1747f12044d78297f42a5f75d7f4e8c97e3
Toby Kreiman <tobykreiman@gmail.com>
2018-11-18 17:58:08 +0000

    hanoi

---
+++ Unit3/hanoi.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <stdlib.h>
+
+// Implement an algorithm that prints out the moves to solve Towers of Hanoi
+// https://www.mathsisfun.com/games/towerofhanoi.html
+// There will be one command line argument saying the starting number of disks
+// Make sure to check for invalid arguments
+
+void MoveTower(int disk, string start, string end, string open);
+
+
+
+int main(int argc, char *argv[])
+{
+    // Check for invalid arguments
+    MoveTower(num_disks, "A", "B", "C")
+}
